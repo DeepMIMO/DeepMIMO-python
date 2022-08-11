9b6c77987d39231a5db1142db1155e81b736b1a6
Umut Demirhan <demirhan.umut@gmail.com>
2022-08-11 00:02:23 -0700

    Generator class for Sionna is added

--- DeepMIMO/__init__.py
+++ DeepMIMO/__init__.py
@@ -1,3 +1,4 @@
 from DeepMIMO.generator import generate_data
 from DeepMIMO.params import default_params
 from DeepMIMO.construct_deepmimo import ant_indices, array_response
+from DeepMIMO.sionna_generator import DeepMIMOGenerator
